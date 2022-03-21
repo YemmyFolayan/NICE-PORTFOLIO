4f030583d2af5fce561d152d9cd4759ea3f5d377
Macbook <macbook@Macbooks-MacBook-Pro.local>
2022-03-21 13:41:01 +0100

    carousel and multiple pages

--- src/js/app.js
+++ src/js/app.js
@@ -24,3 +24,20 @@ function navBarFunction() {
     x.className = "topnav";
   }
 }
+
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 6000); // Change image every 6 seconds
+}
